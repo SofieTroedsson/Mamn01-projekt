b120b6fa8f39c65d4b56368b794dc8a85ce9c75d
SofieTroedsson <Sofie.troedsson.310@student.lu.se>
2015-04-23 11:42:52 +0200

    framsida

--- app/src/main/java/com/example/sofietroedsson/tax/MainActivity.java
+++ app/src/main/java/com/example/sofietroedsson/tax/MainActivity.java
@@ -19,11 +19,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
         Button button4 = (Button) findViewById(R.id.button4);
         button.setOnClickListener(this);
         button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
         button4.setOnClickListener(this);
 
     }
@@ -37,18 +35,11 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 vib.vibrate(500);
                 break;
             }
-            // fel
 
             case R.id.button2: {
                 startActivity(new Intent(this, Options.class));
                 vib.vibrate(500);
                 break;
-            }
-            case R.id.button3: {
-                startActivity(new Intent(this, Test.class));
-                vib.vibrate(500);
-                break;
-
             }
             case R.id.button4:{
                 startActivity(new Intent(this, highScore.class));
